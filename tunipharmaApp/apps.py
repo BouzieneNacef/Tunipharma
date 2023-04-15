685aebe44e9c996c39c4cf589e481e6fceace9e5
BouzieneNacef <bouzaienne1995@gmail.com>
2023-04-15 10:28:16 +0200

    first commit

---
+++ tunipharmaApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TunipharmaappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tunipharmaApp'
---
+++ tunipharmaApp/models.py
@@ -0,0 +1,78 @@
+from datetime import date
+from django.utils import timezone
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50, default='')
+    password = models.CharField(max_length=50, default='')
+    email = models.EmailField(default='')
+    phone = models.TextField(max_length=20, default='')
+    connectionStatus = models.CharField(max_length= 20, default='')
+    InscriptionDate  = models.DateField(default=date(2023,1,1))
+    class Meta:
+        abstract = True
+        ordering = ['email']
+
+    def __str__(self):
+        return f'name={self.name}, email={self.email}, phone ={self.phone},'
+
+class Product(models.Model):
+    label = models.CharField(max_length=20, default='')
+    price = models.FloatField(default=0)
+    stock = models.PositiveSmallIntegerField(default=0)
+    image = models.ImageField(upload_to='image/product_image',null=True, blank=True )
+    description = models.TextField(null=True, blank=True)
+    expirationDate = models.DateField(default=date(2023,12,31))
+    fabricationDate = models.DateField(default=timezone.now())
+
+class Client(User):
+    firstName = models.CharField(max_length=50, default='')
+    lastName = models.CharField(max_length=50, default='')
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=20, default='+21600000000')
+    birthdate = models.DateField(default=date(1995,11,11))
+   # typeClient = models.CharField(max_length=50, choices=[('LOYAL','Loyal Customer'),
+    #('Normall','Normal Customer'),('VIP',' Vip Customer')], default='Normal')
+    # Relationship:
+    clientProduct = models.ManyToManyField(Product, through='Command', through_fields=('client', 'product'))
+    class Meta:
+        db_table ='client'
+
+class Panier(models.Model):
+    panierNumber = models.PositiveIntegerField(default=1)
+    
+
+class Command(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE) # utiliser en relation 
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)  # utiliser en relation 
+    date_cmd = models.DateField(default=timezone.now())
+    quality = models.PositiveSmallIntegerField(default=1)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    class Meta:
+        # the table name in database 
+        db_table ='command'
+        #tuples in command table are orered by data_cmd 
+        ordering = ['-date_cmd']
+        # the name of the table in admin panel is command table(a name readable by humans)
+        verbose_name = 'Command table'
+        # the combinition of client, product and data_cmd must be unique
+        unique_together = [('client', 'product', 'date_cmd')]
+
+class CommandDetails(models.Model):
+    commandNumber = models.PositiveIntegerField(default=0)
+    productNumber = models.PositiveIntegerField(default=0)
+    productName = models.CharField(max_length=20, default='')
+    quantity = models.PositiveIntegerField(default=1)
+    unitPrice = models.FloatField(default=0.0)
+    totalPrice = models.FloatField(default=0.0) 
+
+class DeliveryInformation(models.Model):
+    sendingNumber = models.PositiveIntegerField(default=1)
+    Type = models.CharField(max_length=20, default='')
+    Price = models.FloatField(default=0.0)
+    destination = models.TextField(default='Rue, la ville , code de postal')
+
+
